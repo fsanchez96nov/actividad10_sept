ca3c11f93f027237e6dede7784a23a76a5166834
fabian dario sanchez barrios <fabiandsanchezb@hotmail.com>
2021-09-10 00:39:01 -0500

    tilin

---
+++ database/migrations/2021_09_10_043354_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('cedula');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->unsignedBiginterger('idFicha')->nullable();
+            $table->foreign('idFicha')->references('id')->on('fichas');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
